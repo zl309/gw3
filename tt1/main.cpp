dd77c1c95682c05a639fcb2cda1b112e563c830f
parksohyeon <solevi07@naver.com>
2023-10-05 12:05:26 +0900

    10.5

---
+++ tt1/main.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+
+int main()
+{
+	int aa;
+	int bb;
+	int cc;
+	//scanf_s("%d %d %d", &aa, &bb, &cc);
+	int arrA[3] = {0 };
+	int* parrA =arrA;;
+	int aA = arrA[0];
+	 
+	printf("%d", parrA);
+
+
+}
